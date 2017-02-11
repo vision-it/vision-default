7d57f0ea93feb5952dc73496d7b58a6ac662827a
Markus Opolka <markus@martialblog.de>
2017-02-11 18:14:00 +0100

    Add dynamic backup_port fact
    - Can be set via parameter or gets calculated

--- manifests/facts.pp
+++ manifests/facts.pp
@@ -13,8 +13,10 @@
 
 class vision_default::facts (
 
-  $tier = hiera('applicationtier', 'production'),
-  $type = hiera('nodetype', 'server'),
+  String $tier                  = hiera('applicationtier', 'production'),
+  String $type                  = hiera('nodetype', 'server'),
+  Optional[String] $backup_port = $vision_default::backup_port,
+  String $location              = $vision_default::location,
 
 ){
 
@@ -26,6 +28,30 @@ class vision_default::facts (
   file { '/opt/puppetlabs/facter/facts.d/nodetype.txt':
     ensure  => present,
     content => "nodetype=${type}",
+  }
+
+  # Set backup_port or calculate it only in VMs
+  if $location =~ /(dmz|int)Vm/ {
+
+    if $backup_port == undef {
+
+      $ip_array         = split($::ipaddress, '\.')
+      $ip_last_triple   = $ip_array[-1]
+      $calc_backup_port = "23${ip_last_triple}"
+
+      file { '/opt/puppetlabs/facter/facts.d/backup_port.txt':
+        ensure  => present,
+        content => "backup_port=${calc_backup_port}",
+      }
+    }
+
+    else {
+
+      file { '/opt/puppetlabs/facter/facts.d/backup_port.txt':
+        ensure  => present,
+        content => "backup_port=${backup_port}",
+        }
+    }
   }
 
 }
